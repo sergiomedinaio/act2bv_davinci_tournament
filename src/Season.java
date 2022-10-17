57bfa6f8da4f7f56f9fd08f5785f1e419082bd82
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-10-17 15:00:42 -0300

    feat: add season class

---
+++ src/Season.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Season{
+    private String name;
+    private Date startDate;
+    private Date endDate;
+    private ArrayList<Match> matchs;
+
+    public void setName(String name){
+        this.name = name;
+        return;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setStartDate(Date startDate){
+        this.startDate = startDate;
+    }
+
+    public Date getStartDate(){
+        return this.startDate;
+    }
+
+    public Date getEndDate(){
+        return this.endDate;
+    }
+
+    public void setEndDate (Date endDate){
+        this.endDate = endDate;
+        return;
+    }
+
+    // este no lo entendí. No tendría que recibir un Match en vez de un date??
+    public ArrayList<Match> getMatchsByDate(Date date){
+        return this.matchs;
+    }
+
+    public void addMatch(Match match){
+        // agregar match a this.matchs
+        return;
+    }
+
+
+
+}
